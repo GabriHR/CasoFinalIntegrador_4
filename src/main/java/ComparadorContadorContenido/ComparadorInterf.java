5d81bf18568a28f279e86c2cec216c171893aebe
GabriHR <ghernrod@myuax.com>
2024-04-09 23:47:35 +0200

    7
    Interfaz ComparadorInterf

---
+++ src/main/java/ComparadorContadorContenido/ComparadorInterf.java
@@ -0,0 +1,7 @@
+package ComparadorContadorContenido;
+
+import java.io.IOException;
+
+public interface ComparadorInterf {
+    void compararArchivos(String archivo1, String archivo2) throws IOException;
+}
